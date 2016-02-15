f63de58ea33af2cf6e42b930b01a730fff973c8f
jcrombez <jeremy.crombez@kitpages.fr>
2016-02-15 16:06:42 +0100

    remove useless tail

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 echo "${CRON_TIME} /backup-scripts/backup-all.sh >> /mysql-backup.log 2>&1" > /crontab.conf
 crontab /crontab.conf
 echo "Running cron job"
-exec cron -f && tail -f /mysql-backup.log
+exec cron -f
