c6efb668b398e6576cf095d58e66ad3d1796f145
jcrombez <jeremy.crombez@kitpages.fr>
2016-02-15 16:04:23 +0100

    better output and simplier env var

--- backup-scripts/backup.sh
+++ backup-scripts/backup.sh
@@ -4,26 +4,22 @@ CONTAINER_NAME=$1
 MYSQL_HOST=$2
 MYSQL_PASSWORD=$3
 MAX_BACKUPS=$4
-BACKUP_NAME=$1_$(date +\%Y-\%m-\%d_\%H\%M\%S).sql
+BACKUP_NAME=$1_$(date +\%Y-\%m-\%d_\%H-\%M-\%S).sql
 
-echo "=> ${CONTAINER_NAME} : Backup started (${BACKUP_NAME})"
+echo "Backup => ${CONTAINER_NAME} : start (${BACKUP_NAME}) with ${MAX_BACKUPS} maximum backups"
 
-if mysqldump -h ${MYSQL_HOST} -u root -p${MYSQL_PASSWORD} --all-databases > /backup/${BACKUP_NAME} ;then
-    echo "=> ${CONTAINER_NAME} : Backup succeeded"
+if mysqldump -h ${MYSQL_HOST} -u root -p${MYSQL_PASSWORD} --all-databases --events > /backup/${BACKUP_NAME} ;then
+    echo "Backup => ${CONTAINER_NAME} : ok"
 else
-    echo "=> ${CONTAINER_NAME} : Backup failed"
+    echo "Backup => ${CONTAINER_NAME} : failed"
     rm -rf /backup/${BACKUP_NAME}
 fi
-
-echo "max backups : ${MAX_BACKUPS}"
 
 if [ -n "${MAX_BACKUPS}" ]; then
     while [ $(ls /backup -N1 | wc -l) -gt ${MAX_BACKUPS} ];
     do
-        BACKUP_TO_BE_DELETED=$(ls /backup -N1 | sort | head -n 1)
-        echo "=> ${CONTAINER_NAME} : Backup ${BACKUP_TO_BE_DELETED} is deleted"
-        rm -rf /backup/${BACKUP_TO_BE_DELETED}
+        BACKUP_TO_DELETE=$(ls /backup -N1 | sort | head -n 1)
+        rm -rf /backup/${BACKUP_TO_DELETE}
+        echo "Backup => ${CONTAINER_NAME} : ${BACKUP_TO_DELETE} deleted"
     done
 fi
-
-echo "=> ${CONTAINER_NAME} : Backup done"
--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-env > currentenv
-echo "${CRON_TIME} /backup-scripts/backup-all.sh >> /mysql_backup.log 2>&1" > /crontab.conf
+echo "${CRON_TIME} /backup-scripts/backup-all.sh >> /mysql-backup.log 2>&1" > /crontab.conf
 crontab /crontab.conf
-echo "=> Running cron job"
-exec cron -f
+echo "Running cron job"
+exec cron -f && tail -f /mysql-backup.log
